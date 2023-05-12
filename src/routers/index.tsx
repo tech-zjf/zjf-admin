9ae1e497b5d95846f267303d27febd0b359797a6
tech-zjf <907572438@qq.com>
2023-05-12 14:48:29 +0800

    fix: bug

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,7 +1,10 @@
 import { lazy } from "react";
 
+/** 登录 */
 const LoginPage = lazy(() => import('../page/login'));
+/** 入口页面 */
 const LayoutPage = lazy(() => import('../page/layout'));
+/** 入口页面 - 子页面 */
 const ArticlePage = lazy(() => import('../page/article'));
 const VideoPage = lazy(() => import('../page/video'));
 const PostPage = lazy(() => import('../page/post'));
