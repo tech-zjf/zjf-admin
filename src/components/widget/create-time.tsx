fffdc2d6dfffb0c1a1ddaae928c994a404db6af2
tech-zjf <907572438@qq.com>
2023-06-14 16:05:24 +0800

    fix: 时间格式处理

--- src/components/widget/create-time.tsx
+++ src/components/widget/create-time.tsx
@@ -1,4 +1,6 @@
+import { memo, useMemo } from "react";
 import Icon from "./icon";
+import moment from 'moment'
 
 interface CreateTimeProps {
     createTime: string
@@ -6,11 +8,14 @@ interface CreateTimeProps {
 
 const CreateTime: React.FC<CreateTimeProps> = (props) => {
     const { createTime } = props
+
+    const formattedTime = useMemo(() => moment(createTime).format('YYYY-MM-DD HH:mm:ss'), [])
+
     return (
         <div className="flex items-center text-gray-900">
             <Icon type="icon-time" className="text-base mr-1 flex items-center" />
-            <p className="  text-xs">{createTime}</p>
+            <p className="  text-xs">{formattedTime}</p>
         </div>
     )
 }
-export default CreateTime;
+export default memo(CreateTime);
