ff0c7e7ff9380de77df60e9b88917a45a81f6aab
tech-zjf <907572438@qq.com>
2023-06-20 12:01:27 +0800

    fix: 去掉表情

--- src/components/global/views/view-input.tsx
+++ src/components/global/views/view-input.tsx
@@ -2,29 +2,39 @@ import { Button } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import { useState } from "react";
 
-const ViewInput: React.FC = () => {
+interface ViewInputProps {
+    onCreateView: (params: any) => void
+}
+
+const ViewInput: React.FC<ViewInputProps> = (props) => {
+    const { onCreateView } = props
+
     const [viewStep, setViewStep] = useState({
         content: '',
         pics: [],
     })
 
-    /** 
-     * 创建评论 
-     */
-    const createView = () => {
-    };
-
     return (
         <div>
-            <TextArea rows={4} onChange={(e) => {
-                console.log(e)
-            }} />
+            <TextArea
+                rows={4}
+                onChange={(e) => {
+                    const str = e.target.value
+                    setViewStep({ ...viewStep, content: str })
+                }}
+            />
             <div className="mt-4 flex justify-between items-center">
                 <div className="flex items-center text-sm">
                     <p>图片上传</p>
                 </div>
                 <div className="flex items-center">
-                    <Button onClick={createView}>发布</Button>
+                    <Button
+                        onClick={() => {
+                            onCreateView(viewStep)
+                        }}
+                    >
+                        发布
+                    </Button>
                 </div>
             </div>
         </div>
--- src/components/global/views/view-item.tsx
+++ src/components/global/views/view-item.tsx
@@ -7,45 +7,50 @@ import ViewInput from "./view-input";
 import { ViewItemResponse } from "@/api/modules/views/interface";
 
 interface ViewItemProps {
-  item: ViewItemResponse;
+    item: ViewItemResponse;
 }
 
 const ViewItem: React.FC<ViewItemProps> = (props) => {
-  const { item } = props;
-  return (
-    <>
-      <div className="flex my-6">
-        <div>
-          <Avatar src={item.author.wechatAvatarUrl} shape="circle" size={32} />
-        </div>
-        <div className="ml-4">
-          <p className="text-md font-medium text-gary-900 h-8 flex items-center">
-            {item.author.nickname}
-          </p>
-          <div className="mt-2 text-gray-900 text-sm">
-            阻塞IO怎么是第一个大错误？
-            虽然很多时候是需要异步处理，但也有很多不需要异步处理的啊。它提供了这些个方法，就表示他有使用的场景。请不要以偏盖全。
-            不管什么代码，只有合理的使用，都是重要的。
-          </div>
-          <div className="flex items-center justify-between mt-3">
-            <CreateTime createTime={item.createTime} />
-            <div className=" flex items-center">
-              <div className="mr-6">
-                <LikeIcon isLike={item.isLike} likeCount={item.likeCount} />
-              </div>
-              <div>
-                <ViewIcon viewCount={item.viewCount} />
-              </div>
+    const { item } = props;
+
+    const onCreateView = (params: any) => {
+        console.log(params)
+    }
+
+    return (
+        <>
+            <div className="flex my-6">
+                <div>
+                    <Avatar src={item.author.wechatAvatarUrl} shape="circle" size={32} />
+                </div>
+                <div className="ml-4">
+                    <p className="text-md font-medium text-gary-900 h-8 flex items-center">
+                        {item.author.nickname}
+                    </p>
+                    <div className="mt-2 text-gray-900 text-sm">
+                        阻塞IO怎么是第一个大错误？
+                        虽然很多时候是需要异步处理，但也有很多不需要异步处理的啊。它提供了这些个方法，就表示他有使用的场景。请不要以偏盖全。
+                        不管什么代码，只有合理的使用，都是重要的。
+                    </div>
+                    <div className="flex items-center justify-between mt-3">
+                        <CreateTime createTime={item.createTime} />
+                        <div className=" flex items-center">
+                            <div className="mr-6">
+                                <LikeIcon isLike={item.isLike} likeCount={item.likeCount} />
+                            </div>
+                            <div>
+                                <ViewIcon viewCount={item.viewCount} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-6">
+                        <ViewInput onCreateView={onCreateView} />
+                    </div>
+                    {item?.children?.length && <Views items={item.children} />}
+                </div>
             </div>
-          </div>
-          <div className="mt-6">
-            <ViewInput />
-          </div>
-          {item?.children?.length && <Views items={item.children} />}
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default ViewItem;
