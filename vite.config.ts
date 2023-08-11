577107912247c1c0ce041526af40f2062005cefc
tech-zjf <907572438@qq.com>
2023-08-11 17:01:02 +0800

    fix: bug

--- vite.config.ts
+++ vite.config.ts
@@ -20,12 +20,5 @@ export default defineConfig({
   },
   server: {
     port: 4000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3008",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
   },
 });
